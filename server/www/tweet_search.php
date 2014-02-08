1aa113b03e51c9bd02476aa91f068bd0a6d0548f
Ubuntu <ubuntu@ip-172-31-10-69.us-west-2.compute.internal>
2014-02-08 23:52:26 +0000

    debug

--- server/www/tweet_search.php
+++ server/www/tweet_search.php
@@ -1,9 +1,9 @@
 <?php
 require_once("../lib/twitteroauth.php");
-$consumerKey = "noPOyyZSAyGZgCCRWDWc5g";
-$consumerSecret = "Dk3UXsGGjtsdPaQU142RCXh3yDBjr9xtatESLqDLO1c";
-$accessToken = "104060917-Uo1ncenaWL91GZD6FpQHnx9zjsRjorv2yeTeNFMZ";
-$accessTokenSecret = "7w1p4YuY8KyfgjHL6HQ2k69jSGfDyt0NqvkfmKUQtauM5";
+$consumerKey = "";
+$consumerSecret = "";
+$accessToken = "";
+$accessTokenSecret = "";
 
 $twObj = new TwitterOAuth($consumerKey,$consumerSecret,$accessToken,$accessTokenSecret);
 
